61783c990ea008b57b21a02f32dd1051230ef987
YusufDagdeviren <ddyusufdd@gmail.com>
2024-01-08 17:57:47 +0300

    4

---
+++ pointer/pointer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(void){
+    int y = 5;
+    int *ptr;
+    ptr = &y;
+    printf("%d\n",*ptr);
+    printf("first address is %p\n",ptr);
+    *ptr = 15;
+    printf("%d\n",*ptr);
+    printf("%d\n",y); // pointerın adresindeki değeri değiştirdiğimiz için y de değişti
+    printf("second address is %p\n",ptr);
+    int x = 10;
+    ptr = &x;
+    printf("%d\n",*ptr);
+    printf("third address is %p\n",ptr);
+    return 0;
+}
