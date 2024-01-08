49465d114cddaad99a7378ec759a7ac08c196d65
YusufDagdeviren <ddyusufdd@gmail.com>
2024-01-08 16:35:18 +0300

    2

---
+++ arrays_of_structures/arrays_od_structures.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h> // string fonksiyonlarını kullanabilmek için eklenen kütüphane
+
+struct entry {
+    char fname[10];
+    char lname[12];
+    char phone[8];
+};
+
+int main(void) {
+    struct entry list[10];
+    
+    for (int i = 0; i < 10; i++) {
+        sprintf(list[i].fname, "Yusuf %d", i);
+        sprintf(list[i].lname, "Dagdeviren %d", i);
+        sprintf(list[i].phone, "0555 %d", i);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%s %s %s\n", list[i].fname, list[i].lname, list[i].phone);
+    }
+    
+    return 0;
+}
